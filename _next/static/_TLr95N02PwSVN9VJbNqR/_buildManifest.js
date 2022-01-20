e7dad227a735b02878af3990b7bb122479268ffb
ChrisElyod <ChrisElyod@users.noreply.github.com>
2022-01-20 16:28:57 +0000

    deploy: 16407665ac4a726f7d66370ed6946affea827e3f

--- _next/static/_TLr95N02PwSVN9VJbNqR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cabca10ca472bc80.css","static/chunks/pages/index-a55b2a151d1abef8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
