d0ed006a9f22462239fb99a5b271df4c84417142
ChrisElyod <sean.chris.doyle@gmail.com>
2022-01-20 14:20:38 -0500

    Adding changes for column sorting and debounce search

--- components/book-table/book-table.js
+++ components/book-table/book-table.js
@@ -5,8 +5,39 @@ const BookTable = ({ query }) => {
   const [books, setBooks] = useState([]);
   const [currentSort, setCurrentSort] = useState({ column: '', direction: '' });
 
+  const getSortIcon = (columnToSort) => {
+    if (columnToSort === currentSort.column && currentSort.direction === "asc") {
+      return <>&#9650;</>
+    } else if (columnToSort === currentSort.column && currentSort.direction === "desc") {
+      return <>&#9660;</>
+    }
+    return;
+  }
+
   useEffect(() => {
-    if (!query) return;
+    const booksToUpdate = [...books];
+    booksToUpdate.sort((f,s) => {
+      switch(currentSort.direction) {
+        case "asc":
+          if (f[currentSort.column] < s[currentSort.column]) return -1;
+          if (f[currentSort.column] > s[currentSort.column]) return 1;
+          return 0;
+        case "desc":
+          if (f[currentSort.column] > s[currentSort.column]) return -1;
+          if (f[currentSort.column] < s[currentSort.column]) return 1;
+          return 0;
+        default:
+          break;
+      }
+    })
+    setBooks(booksToUpdate);
+  }, [currentSort])
+
+  useEffect(() => {
+    if (!query) {
+      setBooks([]);
+      return;
+    }
     fetch(`https://openlibrary.org/search.json?q=${query}&fields=author_name,title,cover_i,first_publish_year`)
       .then((res) => {
         if (res.ok) {
@@ -14,30 +45,47 @@ const BookTable = ({ query }) => {
         }
         throw res;
       }).then(data => {
-        console.log(data.docs)
         setBooks(data.docs);
       }).catch(err => console.error(err));
   }, [query]);
 
+  if (!books.length) {
+    return <p className={styles["fea-no-books"]}>No Books to Display...</p>;
+  }
+
   return (
-    <table className={styles["book-table"]} cellSpacing={0}>
+    <table className={styles["fea-book-table"]} cellSpacing={0}>
       <colgroup>
         <col span="1" style={{ width: "20%" }} />
-        <col span="1" style={{ width: "35%" }} />
+        <col span="1" style={{ width: "30%" }} />
         <col span="1" style={{ width: "20%" }} />
-        <col span="1" style={{ width: "20%" }} />
+        <col span="1" style={{ width: "25%" }} />
       </colgroup>
       <thead>
         <tr>
-          <th>Title</th>
+          <th 
+            onClick={() => setCurrentSort({
+              column: 'title',
+              direction: currentSort.column === 'title' && currentSort.direction === "asc" ? "desc" : "asc"
+            })}
+          >
+            Title {getSortIcon('title')}
+          </th>
           <th>Book Cover</th>
           <th>Author</th>
-          <th>Published Date</th>
+          <th
+            onClick={() => setCurrentSort({
+              column: 'first_publish_year',
+              direction: currentSort.column === 'first_publish_year' && currentSort.direction === "asc" ? "desc" : "asc"
+            })}
+          >
+            Published Date {getSortIcon('first_publish_year')}
+          </th>
         </tr>
       </thead>
       <tbody>
-        {books && books.map((book) =>
-          <tr key={book.author_name + book.first_publish_year}>
+        {books && books.map((book, index) =>
+          <tr key={index + (book.author_name || "")}>
             <td
               style={{ borderBottom: 'black solid 1px', textAlign: "center", paddingTop: "1em", paddingBottom: "1em" }}
               tabIndex={0}
@@ -57,7 +105,7 @@ const BookTable = ({ query }) => {
               style={{ borderBottom: 'black solid 1px', textAlign: "center", paddingTop: "1em", paddingBottom: "1em" }}
               tabIndex={0}
             >   
-              {book.author_name}
+              {book.author_name || "Not Available..."}
             </td>
             <td
               style={{ borderBottom: 'black solid 1px', textAlign: "center", paddingTop: "1em", paddingBottom: "1em" }}
--- components/input/input.js
+++ components/input/input.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import styles from './input.module.scss';
+import _ from 'lodash';
 
 const Input = ({ onChange, label, style }) => {
+  const delayedQuery = useRef(_.debounce(q => onChange(q), 500)).current;
+  // const onChangeDebounce = (event) => _.debounce(() => console.log(event), 500);
+
+  const handleChange = (event) => {
+    delayedQuery(event);
+  }
+
   return (
     <div className={styles["fea-input-wrapper"]} style={style}>
       <input
@@ -9,7 +17,7 @@ const Input = ({ onChange, label, style }) => {
         name="bookSearch"
         placeholder={ label }
         id="bookSearch"
-        onChange={onChange}
+        onChange={handleChange}
         type="text"
         tabIndex="0"
       />
